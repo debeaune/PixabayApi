da1afe590f787abe9cb44478757ebfab91a8f03b
marie-laure Debeaune <ml.debeaune@gmail.com>
2022-10-25 16:14:10 +0200

    creation du composant ImageSearch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
   
   const [images, setImages] = useState([]);
-  const [loading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('');
 
   useEffect(() => {
@@ -16,10 +17,11 @@ function App() {
       setIsLoading(false);
     })
     .catch(err => console.log(err));
-  }, []);
+  }, [term]);
 
   return (
     <div className= "container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)}/>
       {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>:
       <div className="grid grid-cols-3 gap-4">
         {images.map(image => (
