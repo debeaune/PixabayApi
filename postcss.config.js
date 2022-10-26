7339c156b402e3ee3dbf7e35f3405978e86377d8
marie-laure Debeaune <ml.debeaune@gmail.com>
2022-10-26 20:29:58 +0200

    mise à jour definitive

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     }
   }
-=======
-   plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-   }
-}
->>>>>>> 2d4e110b744573ad74e22624bbb459187ed1ed62
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
     content: ["./src/**/*.{html,js}"],
-=======
-  content: ["./src/**/*.{html,js}"],
->>>>>>> 2d4e110b744573ad74e22624bbb459187ed1ed62
   theme: {
     extend: {},
   },
   plugins: [],
 }
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 2d4e110b744573ad74e22624bbb459187ed1ed62
